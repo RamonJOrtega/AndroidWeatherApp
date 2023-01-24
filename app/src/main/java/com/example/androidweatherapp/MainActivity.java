21cd2257c614d8718d8caef77fa45f271fe8a1d7
Ramon Ortega <ramon.justis.ortega@gmail.com>
2023-01-23 19:12:31 -0800

    adds picture of app in action

--- app/src/main/java/com/example/androidweatherapp/MainActivity.java
+++ app/src/main/java/com/example/androidweatherapp/MainActivity.java
@@ -45,12 +45,9 @@ public class MainActivity extends AppCompatActivity {
         btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "You clicked me 3", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-
 
     }
 
