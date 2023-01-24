4dc516ad9871d11444fbc6c62cd4dbb69cc62149
Ramon Ortega <ramon.justis.ortega@gmail.com>
2023-01-23 18:19:12 -0800

    adds clickability to the 3 buttons. app will now respond with text verifying click

--- app/src/main/java/com/example/androidweatherapp/MainActivity.java
+++ app/src/main/java/com/example/androidweatherapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.androidweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,14 +15,42 @@ public class MainActivity extends AppCompatActivity {
     EditText et_dataInput;
     ListView lv_weatherReport;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // assign some values to all the controls on layout
         btn_cityID = findViewById(R.id.btn_getCityID);
         btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
         btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+        //these are click listeners for the buttons
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
 
