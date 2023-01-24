2c54a6b7c0f88313848787e523f52854d64212eb
Ramon Ortega <ramon.justis.ortega@gmail.com>
2023-01-23 16:21:59 -0800

    adds to app 3 buttons: get city id, get weather by id, and get weather by by name

--- app/src/main/java/com/example/androidweatherapp/MainActivity.java
+++ app/src/main/java/com/example/androidweatherapp/MainActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
     }
+
+
 }
